87105a87c8ca4f5e06d5165926a79872b5ff4995
Jonatan Magnus Bakke <cph-jb281@cphbusiness.dk>
2018-04-15 16:18:36 +0200

    Split up classes in native

--- CA3nativepart/App.js
+++ CA3nativepart/App.js
@@ -2,77 +2,11 @@ import React from 'react';
 import { Text, View, Platform, TouchableOpacity, StyleSheet, Button, WebView, FlatList, TextInput } from 'react-native';
 import { Constants, WebBrowser } from "expo";
 import { StackNavigator } from 'react-navigation';
+import allUsers from './screens/allUsers';
+import allSwappi from './screens/allSwappi';
+import oneUser from './screens/oneUser';
+import oneSwappi from './screens/oneSwappi';
 
-
-class Login extends React.Component {
-  static navigationOptions = { title: "Login" }
-  render() {
-    return (<View><Text>This will be the Login screen</Text></View>)
-  }
-}
-
-class Logout extends React.Component {
-  static navigationOptions = { title: "Logout" }
-  render() {
-    return (<View><Text>This will be the Logout screen</Text></View>)
-  }
-}
-
-class Swappi extends React.Component {
-  state = {
-    data: []
-  }
-
-  componentWillMount() {
-    this.fetchData();
-  }
-
-  fetchData = async () => {
-    const response = await fetch("http://swapi.co/api/people");
-    const json = await response.json();
-    this.setState({ data: json.results });
-  }
-
-  static navigationOptions = { title: "Swappi" }
-  render() {
-    return (
-      <View>
-        <FlatList
-          data={this.state.data}
-          keyExtractor={(x, i) => i}
-          renderItem={({ item }) => <Text>{item.name}</Text>}
-        />
-      </View>
-    )
-  }
-}
-
-class AdminSwappi extends React.Component {
-  state = {
-    data: {}
-  }
-
-  componentWillMount() {
-    this.fetchData("1");
-  }
-
-  fetchData = async (personId) => {
-    const response = await fetch("http://swapi.co/api/people/" + personId);
-    const json = await response.json();
-    this.setState({ data: json });
-  }
-
-  static navigationOptions = { title: "SwappiAdmin" }
-  render() {
-    return (
-      
-      <View>
-        <TextInput placeholder="Type number from 1 -8" onChangeText={(text) => this.fetchData(text)}/>
-        <Text>{this.state.data.name}</Text>
-      </View>
-    )
-  }
-}
 
 const Touchable = (props) => (
   <TouchableOpacity style={styles.button} onPress={props.onPress}>
@@ -88,10 +22,10 @@ class HomeScreen extends React.Component {
     return (
       <View >
         <Text style={{ textAlign: "center", fontSize: 20 }}>See all Demos implemented by XXXX</Text>
-        <Touchable onPress={() => navigate('login')} title="Login" />
-        <Touchable onPress={() => navigate('logout')} title="Logout" />
-        <Touchable onPress={() => navigate('swappi')} title="Swappi" />
-        <Touchable onPress={() => navigate('adminswappi')} title="Admin Swappi" />
+        <Touchable onPress={() => navigate('allusers')} title="Get all users from backend" />
+        <Touchable onPress={() => navigate('oneuser')} title="Get one user from the backend" />
+        <Touchable onPress={() => navigate('swappi')} title="Get all people from Swappi" />
+        <Touchable onPress={() => navigate('adminswappi')} title="Get one person from Swappi" />
       </View>
     )
   }
@@ -108,12 +42,14 @@ export default class App extends React.Component {
   }
 }
 
+
+
 const RouteStack = StackNavigator({
   Home: { screen: HomeScreen },
-  login: { screen: Login },
-  logout: { screen: Logout },
-  swappi: { screen: Swappi },
-  adminswappi: { screen: AdminSwappi },
+  allusers: { screen: allUsers },
+  oneuser: { screen: oneUser },
+  swappi: { screen: allSwappi },
+  adminswappi: { screen: oneSwappi },
 });
 
 const styles = StyleSheet.create({
